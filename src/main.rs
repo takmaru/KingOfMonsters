88a1557a243f6ecc94f3cf54acfcd4ec7c394a8b
takmaru <takmaru_0905@yahoo.co.jp>
2025-10-23 09:59:37 +0900

    ステータスを取得して表示した

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use std::env;
 use std::path::Path;
+use std::collections::HashMap;
 use rusqlite::{params, Connection, Result};
+
+struct StatusRow {
+    monster_id: i32,
+    status_id: i32,
+    name: String,
+    value: i32,
+}
+
+#[derive(Debug, Clone)]
+struct Status {
+    status_id: i32,
+    name: String,
+    value: i32,
+}
 
 #[derive(Debug)]
 struct Monster {
+    id: i32,
     name: String,
     physical: i32,
     race: String,
     condition: String,
     rank: String,
     age: i32,
+    status: Vec<Status>,
 }
 
 fn main() -> Result<()> {
@@ -17,16 +34,39 @@ fn main() -> Result<()> {
 
     let conn = Connection::open(Path::new(&env::var("PROGRAMDATA").unwrap()).join("TakmaruApp").join("KingOfMonsters").join("SQLite").join("KOM.db"))?;
 
+    let mut monster_status_map: HashMap<i32, Vec<Status>> = HashMap::new();
+    {
+        let mut stmt = conn.prepare("SELECT * FROM monster_status_view")?;
+        let monster_status_view = stmt.query_map([], |row| {
+            Ok(StatusRow {monster_id: row.get(0)?, status_id: row.get(1)?, name: row.get(2)?, value: row.get(3)?})
+        })?;
+        for status_row in monster_status_view {
+            let st = status_row?;
+            monster_status_map
+                .entry(st.monster_id)
+                .or_insert_with(Vec::new)
+                .push(Status{ status_id: st.status_id, name: st.name, value: st.value });
+        }
+    }
+    let monster_status_map = monster_status_map; // イミュータブルにする
+
     let mut stmt = conn.prepare("SELECT * FROM monster_view")?;
     let monsters = stmt.query_map([], |row| {
-        Ok(Monster {name: row.get(0)?, physical: row.get(1)?, race: row.get(2)?,
-            condition: row.get(3)?, rank: row.get(4)?, age: row.get(5)?,
+        let id = row.get(0)?;
+        Ok(Monster {id, name: row.get(1)?, physical: row.get(2)?, race: row.get(3)?,
+            condition: row.get(4)?, rank: row.get(5)?, age: row.get(6)?, status: monster_status_map.get(&id).cloned().unwrap_or_default(),
         })
     })?;
 
+    // モンスター一覧を表示する
     for monster in monsters {
         println!("{:?}", monster?);
     }
+
+    // 育成するモンスターを選択する
+    // -> 1
+
+    // モンスターのステータスを取得する
 
 /*
     let conn = Connection::open_in_memory()?;
